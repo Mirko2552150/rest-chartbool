150082c0b5f69a829f4b6e6913cd716125a12226
Mirko <mirko.longo.1987@gmail.com>
2020-04-07 15:48:17 +0200

    Push x Nik

--- js/main.js
+++ js/main.js
@@ -6,31 +6,35 @@ $.ajax({
         var rispostaJson = data;
         // console.log(rispostaJson);
         var oggettoIntermedio = {};
-        // getRispostaSingola(rispostaJson);
-        // // console.log(rispostaJson);
+        // getRispostaSingola(rispostaJson); // funzione da inserire
+        // console.log(rispostaJson);
         for (var i = 0; i < rispostaJson.length; i++) { // cicliamo l'Arrey di oggetti dalla risposta Json
             var rispostaSingolaJson = rispostaJson[i]; // creiamo il nostro singolo oggetto presente nella chiamata Json
-            var amount = rispostaSingolaJson.amount; // fatturato dei ventiri estrapolati dalla chiamata con ciclo for
-            console.log(amount);
+            var fatturato = rispostaSingolaJson.amount; // fatturato dei ventiri estrapolati dalla chiamata con ciclo for
+            var dataVendita = rispostaSingolaJson.date;
+            var dataVendita = moment(rispostaSingolaJson.date);
+            // console.log(dataVendita);
+            var meseVendita = dataVendita.locale('it').format('MMMM');
+            console.log(meseVendita);
         }
+
     },
     error: function() {
         alert('error')
     }
 });
 
-
 var ctx = $('#grafico-uno');
 var chart = new Chart(ctx, {
     type: 'line',
 
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio'],
         datasets: [{
             label: 'My First dataset',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            data: [0, 10, 5, 2, 20, 30, 45, 0, 0, 0, 0, 15] // 12 data che corrispondono ai mesi
         }]
     },
 });
@@ -49,10 +53,6 @@ var chart = new Chart(ctx, {
         }]
     },
 });
-
-
-
-
 
 // function getRispostaSingola(rispostaJ) {
 //     for (var i = 0; i < rispostaJ.length; i++) {
